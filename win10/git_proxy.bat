ca6f6d64064c033766a0e623e86674310b3a7602
Dongdong Kong <kongdd@users.noreply.github.com>
2020-07-19 21:49:46 +0800

    fix git_proxy

--- win10/git_proxy.bat
+++ win10/git_proxy.bat
@@ -1,2 +1,2 @@
-git config --global --unset http.proxy
-git config --global --unset https.proxy
+git config --global http.proxy 'socks5://127.0.0.1:1081'
+git config --global https.proxy 'socks5://127.0.0.1:1081'
